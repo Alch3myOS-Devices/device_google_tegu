4cf56338237f19fe47c3687867f86bd95ae958a7
Libra420T <ryantf420@gmail.com>
2026-05-07 18:51:30 -0400

    Update flags

--- device-tegu.mk
+++ device-tegu.mk
@@ -5,35 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Disable Artifact Requirements
-DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
-
-# GMS/GAPPS
-WITH_GMS := true
-
-TARGET_SUPPORTED_REFRESH_RATES := 60,120
-
-SURFACE_FLINGER_BOOST := true
-$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
-$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
-
 # Always use scudo for memory allocator
 PRODUCT_USE_SCUDO := true
-
-TARGET_OPTIMIZED_DEXOPT := true
-
-# Build Pixel Launcher
-TARGET_INCLUDE_PIXEL_LAUNCHER := true
-
-# Include Google Telephony
-WITH_GMS_COMMS_SUITE := true
-
-# Include Chargeing Bypass
-BYPASS_CHARGE_SUPPORTED := true
-
-# Supports HBM
-HBM_SUPPORTED := true
-HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
 
 # Pixel Kernel
 TARGET_LINUX_KERNEL_VERSION := 6.1
